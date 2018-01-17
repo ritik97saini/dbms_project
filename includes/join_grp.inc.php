1035ed9769736eeac338dace0033190880d93d2c
cornetto17 <31823083+cornetto17@users.noreply.github.com>
2018-01-18 00:43:17 +0530

    Add files via upload

--- includes/join_grp.inc.php
+++ includes/join_grp.inc.php
@@ -14,35 +14,38 @@ if(isset($_POST['group_id']))
 	$uid=$_SESSION['u_uid'];
 	$sql="select * from group_info where group_id='$gid';";
 	$result=mysqli_query($conn, $sql);
-	$data=mysqli_fetch_assoc($result);
 	$resultlist=mysqli_num_rows($result);
 	if($resultlist>0)
 	{
+		$uid=$_SESSION['u_uid'];
+		$sql="select * from user_student where student_id='$uid';";
+		$result=mysqli_query($conn, $sql);
+		$data=mysqli_fetch_assoc($result);
 		if($data['resume_status']==1)
 		{
 			$sql="select * from group_student where group_id='$gid' and student_id='$uid';";
-		$result=mysqli_query($conn,$sql);
-		$resultlist=mysqli_num_rows($result);
-		if($resultlist==0)
-		{
-			$sql="insert into group_student(group_id,student_id)
-				  values('$gid','$uid');";
-			mysqli_query($conn,$sql);
-			header("Location: ../stu_tab/stu_tab.php?status=sent");
-			exit();
+			$result=mysqli_query($conn,$sql);
+			$resultlist=mysqli_num_rows($result);
+			if($resultlist==0)
+			{
+				$sql="insert into group_student(group_id,student_id)
+					  values('$gid','$uid');";
+				mysqli_query($conn,$sql);
+				header("Location: ../stu_tab/stu_tab.php?status=sent");
+				exit();
+			}
+			else
+			{
+				header("Location: ../stu_tab/stu_tab.php?status=added");
+				exit();
+			}
 		}
 		else
 		{
-			header("Location: ../stu_tab/stu_tab.php?status=added");
+			header("Location: ../stu_tab/stu_tab.php?status=profileupdate");
 			exit();
-		}
-	}
-	else
-	{
-		header("Location: ../stu_tab/stu_tab.php?status=profileupdate");
-		exit();
 
-	}	
+		}	
 
 
 	}
