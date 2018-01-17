1a2018fa1d1d19af8c8399746507408b115dc9c0
RohitKumar1996 <rohitkr.rht78@gmail.com>
2018-01-18 00:34:09 +0530

    Add files via upload

--- stu_tab/stu_tab.php
+++ stu_tab/stu_tab.php
@@ -41,15 +41,166 @@ include '../includes/dbh.inc.php';
     </ul>
     </div>
 </nav>
+        
+        <?php
+        $student=$_SESSION['u_uid'];
+        
+        $sql4="SELECT group_id FROM group_student where student_id='$student';";
+							$query4=mysqli_query($conn,$sql4);
+							$row4=mysqli_fetch_assoc($query4);
+        $id1=$row4['group_id'];
+        
+        
+        if(!empty($_GET['grp']))
+        $id1=$_GET['grp'];
+							$sql2="SELECT grp_name FROM group_info where group_id='$id1';";
+							$query2=mysqli_query($conn,$sql2);
+							$row2=mysqli_fetch_assoc($query2);
+        ?>
 		
 		<div id="wrapper">
 		
 			<main>
 				<div id="content">
 					<div class="innertube">
-						<h1>Heading</h1>
-						
-						
+						<?php
+                        
+                        if(isset($_GET['grp'])) {
+                            $gid = $_GET['grp'];
+                            $sql="select * from group_info where group_id='$gid';";
+							$result=mysqli_query($conn,$sql);
+							$resultlist=mysqli_num_rows($result);
+                            $row = mysqli_fetch_assoc($result);
+                            
+                            ?>
+                          <?php   echo'<div style = "color: white; background-color: #000099; text-align: center; font-size: 40px; font-weight: bold; padding: 10px;">'.$row['grp_name'].'</div>';?>
+                            
+                            <br>
+                            <button type="button" class="btn" id="info">INFO</button>
+                            <button type="button" class="btn" id="post">POSTS</button>
+                            
+                            
+                            <style>
+                            
+                            .grp_table {
+                                margin-left: 200px;
+                                font-family: arial, sans-serif;
+                                border-collapse: collapse;
+                                width: 60%;
+                            }
+
+                            .grp_td {
+                                border: 1px solid #dddddd;
+                                text-align: left;
+                                padding: 8px;
+                            }
+
+                            .grp_tr:nth-child(even) {
+                                background-color: #dddddd;
+                            }
+                            
+                            </style>
+                        
+                        <div id="disp_info" style="display:none;">
+                                <br><br><br>
+                            
+                                <table class = "grp_table">
+            
+                                      <tr class = "grp_tr">
+                                        <td class = "grp_td">Group Name</td>
+                                        <td class = "grp_td"><?php echo $row['grp_name'];?></td>
+                                      </tr>
+                                      <tr class = "grp_tr">
+                                        <td class = "grp_td">Group Code</td>
+                                        <td class = "grp_td"><?php echo $row['group_id'];?></td>
+                                      </tr>
+                                      <tr class = "grp_tr">
+                                        <td class = "grp_td">Description</td>
+                                        <td class = "grp_td"><?php echo $row['description']; ?></td>
+                                      </tr>
+                                      <tr class = "grp_tr">
+                                        <td class = "grp_td">Created On</td>
+                                        <td class = "grp_td"><?php echo $row['created_on']; ?></td>
+                                      </tr>
+                                      
+                                </table>
+                            </div>
+                            
+                            <div id="notice" style="margin-top:80px; ">
+                       
+                        <div id="disp_post"  >
+                            
+                            
+                        <?php 
+							$sql1="SELECT notice_id,type FROM notice  where group_id='$id1' order by created_on desc;";
+							$query1=mysqli_query($conn,$sql1);
+                            
+							while ($row1=mysqli_fetch_assoc($query1)) {
+                                $notice=$row1["notice_id"];
+                                if($row1["type"]==1)
+                                {
+                                    $sql2="select * from notice_1 where notice_id='$notice';";
+                                }
+                                else
+                                {
+                                    $sql2="select * from notice_2 where notice_id='$notice';";
+                                }
+                                $query2=mysqli_query($conn,$sql2);
+                                $row2=mysqli_fetch_assoc($query2);
+                                ?>
+                                
+                            
+                            <div style="
+                                background-color: crimson;
+                                border:10px;
+                                margin: 50px 10px 10px 10px;
+                                border-color:cyan ;
+                                padding-top: 10px;
+                                border-radius: 20px;
+                                ">
+						<center><p style="font-size: 40px; align-content: center">                              
+                               <?php echo $row2["heading"]; ?>
+							</p></center>
+                            <center><p style="font-size: 15px; align-content: center">
+                                
+                              <?php echo $row2["description"]; ?>
+							</p></center>
+                                <br>
+                            </div>
+							
+                        <?php } ?>
+                        
+                        
+                        </div>
+                            </div>
+                        
+                            
+                        <script>
+                            var info = document.getElementById("info");
+                            var post = document.getElementById("post");
+                            
+                            var disp_info = document.getElementById("disp_info");
+                            var disp_post = document.getElementById("disp_post");
+                            
+                            info.onclick = function() {
+                                disp_post.style.display = "none";
+                                
+                                disp_info.style.display = "block";
+                            }
+                            post.onclick = function() {
+                                disp_info.style.display = "none";
+                                
+                                disp_post.style.display = "block";
+                            }
+                           
+                        </script>
+                            
+                
+                        <?php } ?>
+                        
+                        
+                        
+                            
 					</div>
 				</div>
 			</main>
