456f665e6a467c4973a0d2eef0d2f0aa7b566d53
ritik97saini <ritik97saini@gmail.com>
2018-01-19 22:10:55 +0530

    Add files via upload

--- admin_tab/admin_tab.php
+++ admin_tab/admin_tab.php
@@ -13,7 +13,7 @@ $id=$_SESSION['u_uid'];
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>2 Column Layout &mdash; Left Menu with Header &amp; Footer</title>
+		<title>admin_tab</title>
 		<link rel="stylesheet" href="admin_tab.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -22,6 +22,7 @@ $id=$_SESSION['u_uid'];
 	
 	<body>
        
+        
         <div id = "baap">
 		<nav class="navbar navbar-inverse" style="width=100%;">
   <div class="container-fluid" style="widht=100%;">
@@ -34,12 +35,15 @@ $id=$_SESSION['u_uid'];
       <li><a href="admin_profile_display.php">Profile</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
+       
       
       <li><a href="../includes/logout.inc.php" ><span class="glyphicon glyphicon-log-in" ></span> Logout</a></li>
     </ul>
   </div>
 </nav>
         </div>
+        <
+       
  
 					<?php
         $admin=$_SESSION['u_uid'];
@@ -77,21 +81,68 @@ $id=$_SESSION['u_uid'];
 							$resultlist=mysqli_num_rows($result);
                             $row = mysqli_fetch_assoc($result);
                             
-                            echo '
-                            <div style = "color: white; background-color: #000099; text-align: center; font-size: 40px; position:fixed; width:85%; font-weight: bold; padding: 10px;">'.$row['grp_name'].'</div>
+                            ?>
+                            <div style = "color: white; background-color: #000099; text-align: center; font-size: 40px; position:fixed; width:85%; font-weight: bold; padding: 10px;"><?php echo $row['grp_name'];?></div>
                             
                             <br><br><br><br><br>
                             <div id="navi" style="position: fixed; width:85%;">
 <center>
                             <button type="button" class="btn" id="info">INFO</button>
                             <button type="button" class="btn" id="post">POSTS</button>
-                            <button type="button" class="btn" id="member">MEMBERS</button>
+                            <button type="button" class="btn" id="member">MEMBERS<span class="countBubl " id="notif" style="float:right;
+    margin-top:-26px;
+    margin-left:2px;
+    background:#ed1d24;
+    color:#fff;
+    padding:2px;
+    display:none;"></span></button>
 </center>
                       </div>      
                         
-                        <div id="disp_info" style="display:none">'.$row['grp_name'].'<br>'.$row['group_id'].'<br>'.$row['description'] ; ?>
+                            <style>
                             
-                        </div>
+                            .grp_table {
+                                margin-left: 200px;
+                                font-family: arial, sans-serif;
+                                border-collapse: collapse;
+                                width: 60%;
+                            }
+                            .grp_td {
+                                border: 1px solid #dddddd;
+                                text-align: left;
+                                padding: 8px;
+                            }
+                            .grp_tr:nth-child(even) {
+                                background-color: #dddddd;
+                            }
+                            
+                            </style>
+                        
+                        <div id="disp_info" style="display:none;">
+                                <br><br><br>
+                            
+                                <table class = "grp_table">
+            
+                                      <tr class = "grp_tr">
+                                        <td class = "grp_td">Group Name</td>
+                                        <td class = "grp_td"><?php echo $row['grp_name'];?></td>
+                                      </tr>
+                                      <tr class = "grp_tr">
+                                        <td class = "grp_td">Group Code</td>
+                                        <td class = "grp_td"><?php echo $row['group_id'];?></td>
+                                      </tr>
+                                      <tr class = "grp_tr">
+                                        <td class = "grp_td">Description</td>
+                                        <td class = "grp_td"><?php echo $row['description']; ?></td>
+                                      </tr>
+                                      <tr class = "grp_tr">
+                                        <td class = "grp_td">Created On</td>
+                                        <td class = "grp_td"><?php echo $row['created_on']; ?></td>
+                                      </tr>
+                                      
+                                </table>
+                            </div>
+                            
                     <div id="notice" style="margin-top:80px; ">
                        
                         <div id="disp_post"  ">
@@ -121,9 +172,15 @@ $id=$_SESSION['u_uid'];
 							<center><p style="font-size: 40px; align-content: center">                              
                                 <?php echo $row2['heading']; ?>
 							</p></center>
-                            <center><p style="font-size: 15px; align-content: center">
+                            <center><p style="font-size: 15px; align-content: center;">
                                 
-                       Description:       <?php echo $row2['description']; ?>
+                       Description:     <pre style="font-size: 15px; align-content: center;
+                               background-color: crimson;
+                                border:10px;
+                                margin: 50px 10px 10px 10px;
+                                border-color:cyan ;
+                                padding-top: 10px;
+                                border-radius: 20px;" >  <?php echo $row2['description']; ?></pre>
 							</p><center>
                                 <br>
                             </div> 
@@ -198,7 +255,79 @@ $id=$_SESSION['u_uid'];
                         
                         </div>
                             </div>
-                        <div id="disp_member" style="display:none">yay you are in MEMBER!</div>
+                        <div id="disp_member" style="display:none">
+            
+            <?php
+                                $dict=array('computer science' , 'information techonology','electronics and communication engineering',
+                                            'mechanical engineering','civil engineering','biotech engineering','manufacturing and process engineering');
+                                $gid=$_GET["grpid"];
+                                $sql="select * from group_student where group_id='$gid';";
+                                $result=mysqli_query($conn,$sql);
+                                $list=mysqli_num_rows($result);
+                                if($list>0)
+                                {
+                                   
+                                    while($row=mysqli_fetch_assoc($result))
+                                    {   
+                                        $sid=$row['student_id'];
+                                        $sql="select * from user_student where student_id='$sid';";
+                                        $data=mysqli_query($conn,$sql);                                 
+                                        $sdata=mysqli_fetch_assoc($data);
+                                        $img="";
+                                        if($sdata['profilepic_status']==0)
+                                        {
+                                            $img="../stu_tab/uploads/profile_pic/default.jpg";
+                                        }
+                                        else
+                                        {
+                                            $img="../stu_tab/uploads/profile_pic/".$sid."*";
+                                            $p=glob($img);
+                                            $img=$p[0];
+
+                                        }
+
+                                    ?>
+                                     <div style="
+                                background-color: #ff8060;
+                                border:10px;
+                                margin: 50px 10px 10px 10px;
+                                border-color:cyan ;
+                                padding-top: 10px;
+                                padding-left: 10px;
+                                padding-bottom:0px;
+                                width: 80%;
+                                border-radius: 20px;
+                                font-size: 140%;
+                                "> <img src= "<?php echo $img ; ?>" height="100" width="100"><div style="position: relative;top: -105px;left: 150px; height: 15px;"  ><a href="#" > 
+                                <?php 
+                                    
+                                    echo $sdata["firstname"]." ".$sdata["lastname"]." </a><br> ".$sdata['organisation']."<br>".$dict[$sdata['branch']]."<br> ".$sdata['email']." </div>   "; 
+                                    if($row['status']==0)
+                                    {
+                                        echo " <div style='position  : relative; float: right; top :-100px; left: -20px;'><form method='post'  > <button name='accept'> Accept</button>  <button name='reject'>Reject</button></form>
+                                        </div>";
+                                    }
+                                    elseif($row['status']==1)
+                                    {
+                                        echo '<div style="position  : relative; float: right; top :-90px; left: -30px;"><form> <button > Block</button></form></div>';
+                                    }
+                                    elseif($row['status']==2)
+                                    {
+                                        echo '<div style="position  : relative; float: right; top :-90px; left: -30px;"><form> <button > Unblock</button></form></div>';
+                                    }
+                                ?>
+                               
+                                                   
+                                </div>
+
+
+                                 <?php   
+                             }
+                                   
+                                }
+                            ?>
+            
+            </div>
                             <script>
                             
                             var add =document.getElementById('add_notice');
@@ -292,10 +421,31 @@ $id=$_SESSION['u_uid'];
 						
 
 					</script>
+
 				</div>
 			</nav>
 		
 		</div>
+<?php 
+
+            $sq="select count(*) as cou from group_student where status=0 and group_id='$gid';";
+            $que=mysqli_query($conn,$sq);
+            $count1=mysqli_fetch_assoc($que);
+$count=$count1['cou'];
+?>
+
+                    <script>
+var count="<?php echo $count;?>";
+                        
+                        var notif=document.getElementById('notif');
+                        if(count>0)
+                            {
+                                notif.style.display="block";
+                                notif.textContent=count;
+                            }
+
+                    </script>
+ 
 		
 		
 	
